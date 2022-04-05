6b7cb76ed528afa4abb9c583c4559670e72ef9a6
ryanrwei <r.a1996315@gmail.com>
2022-04-05 16:26:02 +0800

    ptt

--- NFT_Spider/src/main/java/NFT_spider.java
+++ NFT_Spider/src/main/java/NFT_spider.java
@@ -10,11 +10,13 @@ public class NFT_spider {
 
         Document doc = Jsoup.connect("https://www.ptt.cc/bbs/Kaohsiung/index.html").get();
 
+//        https://www.ptt.cc/bbs/Kaohsiung/index.html
+
         Elements titles = doc.select("div.r-ent>div.title>a");
         for (Element title : titles) {
-            String linkHref = title.attr("href");
+//            String linkHref = title.attr("href");
             System.out.println(title.text());
-            System.out.println("link:" + "https://www.ptt.cc" + linkHref);
+//            System.out.println("link:" + "https://www.ptt.cc" + linkHref);
         }
     }
 }
---
+++ PTT_Spider/src/main/java/PTT_Spider.java
@@ -0,0 +1,42 @@
+import java.io.IOException;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+
+public class PTT_Spider {
+    public static void main(String[] args) throws IOException {
+
+        Document doc = Jsoup.connect("https://www.ptt.cc/bbs/hotboards.html").get();
+        Elements Board = doc.select("div.b-ent>a.board"); //board
+
+        for (Element Boards : Board) {
+            String BoardName = Boards.select(".board-name").text();
+            System.out.println(BoardName);
+            String BoardsLink = Boards.attr("href");
+
+            Document doc2 = Jsoup.connect("https://www.ptt.cc" + BoardsLink).get();
+            Elements Article = doc2.select("div.r-ent"); // board/article
+            for (Element Articles : Article) {
+                String ArticleName = Articles.select(".title>a").text();
+                System.out.println(ArticleName);
+
+                Elements ArticleLink = Articles.select(".title>a");
+                String ArticleLinks = ArticleLink.attr("href");
+
+                Document doc3 = Jsoup.connect("https://www.ptt.cc" + ArticleLinks).get();
+                System.out.println("Links:" + "https://www.ptt.cc" + ArticleLinks);
+                Elements Comment = doc3.select("div.push>span.f3.push-content"); // board/article/comment
+
+                for (Element Comments : Comment) {
+                    System.out.println(Comments.text());
+                    break;
+                }
+                break;
+            }
+            System.out.println("--------------------------------------");
+        }
+    }
+
+}
